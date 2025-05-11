95b40fcc7d9e721b635c6ad906015c05603a8a18
I4LYT <arnayk@orbical.dev>
2025-05-11 14:52:53 -0500

    Add initial Next.js configuration and setup with Tailwind CSS; include SVG assets and update README for local setup instructions

---
+++ apps/core/envy-core/README.md
@@ -0,0 +1,22 @@
+<img width="480" height="200" alt="Envybase logo" src='https://raw.githubusercontent.com/orbical-dev/.github/refs/heads/main/Envybase%20(240%20x%20100%20px).svg'/>
+
+<br>
+
+# Frontend
+Envybase is an open-source Backend-as-a-Service (BaaS) platform written in Python, designed to simplify and accelerate backend development. It provides a suite of ready-to-use services for common application needs, allowing developers to focus on frontend and business logic.
+
+
+## How to run the frontend locally (Linux only)
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/orbical-dev/envybase.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+    cd envybase/apps/core/envy-core
+    ```
+3. Build and run the app
+    ```bash
+    npm run build && npm run start
+    ```
+4. Visit the app at `http://<your-ip>:3120`
---
+++ apps/core/envy-core/src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Envybase Frontend",
+  description: "Envybase is a self-hostable, open-source alternative to Firebase.",
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon.ico",
+        apple: "/apple-touch-icon.png",
+    },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
