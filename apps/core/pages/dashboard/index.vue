86650f2b348aa5ac0ae7284b226193840b60cc75
I4LYT <arnayk@orbical.dev>
2025-05-13 19:41:22 -0500

    Add initial Nuxt.js setup with Tailwind CSS, create Navbar component, and configure TypeScript

---
+++ apps/core/pages/dashboard/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<template>
+  <body class="text-white flex flex-col min-h-screen">
+  <NuxtLayout>
+    <NuxtPage />
+    <Navbar></Navbar>
+    <div class="content">
+    <h1 class="text-4xl">Dashboard</h1>
+    <p class="">Hi</p>
+    </div>
+  </NuxtLayout>
+  </body>
+</template>
+
+<style scoped>
+  body {
+    background-color: #212121;
+  }
+  .content {
+    margin: 1.5rem;
+  }
+</style>
---
+++ apps/core/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<template>
+  <body class="text-white flex flex-col min-h-screen">
+  <NuxtLayout>
+    <NuxtPage />
+    <Navbar></Navbar>
+  </NuxtLayout>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
