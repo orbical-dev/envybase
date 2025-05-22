66b06ae5af008d5d2583eaf67ee913864d134b89
Zachary Jones <zjones.2012@icloud.com>
2025-05-22 11:15:44 -0500

    Add initial Vue components and settings for Envybase project

---
+++ apps/core/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+<h2>Welcome to Envybase</h2> <button>Database</button>
+</template>
---
+++ apps/core/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>This is the index page, we are still working on it though.</p>
+</template>
